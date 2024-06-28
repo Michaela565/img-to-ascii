3653ee2c749df9efb1bc1aadb5f7967335b8d231
Michaela MeL4 Kurčíková <miskakurcikova82@gmail.com>
2024-06-28 20:28:44 +0200

    show resized grayscale img

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
+import cv2 as cv
+
 def main():
-    pass
+    img = cv.imread("kuromi.jpg", cv.IMREAD_GRAYSCALE)
+    resize_img = cv.resize(img, (200, 200))
+    cv.imshow("picture", resize_img)
+    cv.waitKey(0)
+    cv.destroyAllWindows()
 
 if __name__ == "__main__":
     main()
