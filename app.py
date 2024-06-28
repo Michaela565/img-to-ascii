4ac6af8400ae77ad3285c5095bc4964672545e51
Michaela MeL4 Kurčíková <miskakurcikova82@gmail.com>
2024-06-28 20:48:33 +0200

    loop through the pixels

--- app.py
+++ app.py
@@ -2,7 +2,16 @@ import cv2 as cv
 
 def main():
     img = cv.imread("kuromi.jpg", cv.IMREAD_GRAYSCALE)
+    width = 200
+    height = 200
     resize_img = cv.resize(img, (200, 200))
+
+    for row in range(height):
+        for col in range(width):
+            pixel = resize_img[row, col]
+            print(pixel)
+
+
     cv.imshow("picture", resize_img)
     cv.waitKey(0)
     cv.destroyAllWindows()
