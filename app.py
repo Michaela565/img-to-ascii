9074506d93f38075346790269e52c1ec583eaee7
Michaela MeL4 Kurčíková <miskakurcikova82@gmail.com>
2024-06-29 14:29:17 +0200

    add comments and fix ups

--- app.py
+++ app.py
@@ -1,29 +1,22 @@
 import cv2 as cv
 
 def main():
-    img_path = "kuromi.jpg"
+    img_path = input("What's the path to your image? ")
     img = cv.imread(img_path, cv.IMREAD_GRAYSCALE)
     width = 100
     height = 100
     resize_img = cv.resize(img, (width, height))
-    character_bank = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "=", "[", "]", ":", "{", "}", ";", "'", "|", ",", "<", ">", "?", "/", "~"]
+    char_bank = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "=", "[", "]", ":", "{", "}", ";", "'", "|", ",", "<", ">", "?", "/", "~"]
     for row in range(width):
         pixel_count = 0 # Var to check if we are at the end of the row
         for col in range(height):
             pixel = resize_img[row, col]
-            # print(pixel//10)
-            character_select = pixel // 10
+            char_select = pixel // 10 # Selects which character from the char_bank to use
             if pixel_count == width-1:
-                print(character_bank[character_select])
+                print(char_bank[char_select])
             else:
-                print(character_bank[character_select], end=" ")
+                print(char_bank[char_select], end=" ")
             pixel_count += 1
-            
-
-
-    cv.imshow("picture", resize_img)
-    cv.waitKey(0)
-    cv.destroyAllWindows()
 
 if __name__ == "__main__":
     main()
