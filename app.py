34db869fa322fbbb595adc53d7b4aab5cae96b54
Michaela MeL4 Kurčíková <miskakurcikova82@gmail.com>
2024-06-29 14:23:01 +0200

    do the algorithm

--- app.py
+++ app.py
@@ -2,14 +2,22 @@ import cv2 as cv
 
 def main():
     img = cv.imread("kuromi.jpg", cv.IMREAD_GRAYSCALE)
-    width = 200
-    height = 200
-    resize_img = cv.resize(img, (200, 200))
-
+    width = 100
+    height = 100
+    resize_img = cv.resize(img, (width, height))
+    character_bank = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "=", "[", "]", ":", "{", "}", ";", "'", "|", ",", "<", ">", "?", "/", "~"]
     for row in range(height):
+        pixel_count = 0
         for col in range(width):
             pixel = resize_img[row, col]
-            print(pixel)
+            # print(pixel//10)
+            character_select = pixel // 10
+            if pixel_count == width-1:
+                print(character_bank[character_select])
+            else:
+                print(character_bank[character_select], end=" ")
+            pixel_count += 1
+            
 
 
     cv.imshow("picture", resize_img)
