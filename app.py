6033c95d589737c018d130ea0f7b57c78dce56da
Michaela MeL4 Kurčíková <miskakurcikova82@gmail.com>
2024-06-30 16:16:07 +0200

    write ascii art to file

--- app.py
+++ app.py
@@ -4,7 +4,12 @@ import cv2 as cv
 
 def main():
     img_path = input("What's the path to your image? ")
+    should_file = input("Do you want to save the ASCII art to a file? yes = y, no = any character: ")
+    file_name = ""
+    if should_file == "y":
+        file_name = input("Input the name of the file (If you input the name of an existing file, the data will be overwritten):")
     img = cv.imread(img_path, cv.IMREAD_GRAYSCALE)
+    file = open(file_name + ".txt", "w")
     width = 100
     height = 100
     resize_img = cv.resize(img, (width, height))
@@ -14,11 +19,17 @@ def main():
         for col in range(height):
             pixel = resize_img[row, col]
             char_select = pixel // 10 # Selects which character from the char_bank to use
+            if file_name != "":
+                if pixel_count == width-1:
+                    file.write(char_bank[char_select])
+                    file.write("\n")
+                else:
+                    file.write(char_bank[char_select])
             if pixel_count == width-1:
                 print(char_bank[char_select])
             else:
                 print(char_bank[char_select], end=" ")
             pixel_count += 1
-
+    file.close()
 if __name__ == "__main__":
     main()
